724ac1ccf09acfd9882be4d93cf05850c50a6515
Abraao <abraao101icloud.com@MacBook-Air-de-Abraao.local>
2026-01-14 10:33:36 -0300

    finalizado project

--- mobile/src/app/trip/[id].tsx
+++ mobile/src/app/trip/[id].tsx
@@ -61,6 +61,10 @@ export default function Trip() {
     try {
       setIsLoadindTrip(true);
 
+      if (tripParams.participant) {
+        setShowModal(MODAL.CONFIRM_ATTENDANCE);
+      }
+
       if (!tripParams.id) {
         return router.back();
       }
@@ -175,6 +179,27 @@ export default function Trip() {
     }
   }
 
+
+  async function handleRemoveTrip(){
+    try {
+      Alert.alert("Remover viagem","Tem certeza que deseja remover a viagem?", [
+        {
+          text: "Não",
+          style: "cancel"
+        },
+        {
+          text: "Sim",
+          onPress: async () => {
+            await tripStorage.remove()
+            router.navigate("/")
+          }
+        }
+      ])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     getTripDetails();
   }, []);
@@ -263,6 +288,10 @@ export default function Trip() {
           <Button onPress={handleUpdateTrip} isLoading={isUpdatingTrip}>
             <Button.Title>Atualizar</Button.Title>
           </Button>
+
+          <TouchableOpacity activeOpacity={0.8} onPress={handleRemoveTrip}>
+            <Text className="text-red-400 text-center mt-6">Remover viagem</Text>
+          </TouchableOpacity>
         </View>
       </Modal>
 
